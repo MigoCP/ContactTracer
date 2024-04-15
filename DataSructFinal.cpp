b649e3584d76a814d3675a9c7cec153e870cdd87
Tu Anh <june265zz@gmail.com>
2024-04-15 12:45:56 -0400

    Added the children list to the TreeNode class

--- DataSructFinal.cpp
+++ DataSructFinal.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <list>
 
 using namespace std;
 
@@ -8,99 +9,82 @@ private:
     string medicareId;
     int directContacts = 0;
     int totalCases = 1;
-    TreeNode* parent;
-    //list<TreeNode> tree;
+    TreeNode *parent;
+    list<TreeNode *> children;
 
 public:
-    TreeNode(const string& medId) 
-    { 
-        medicareId = medId; 
-        parent = nullptr; 
+    TreeNode(const string &medId)
+    {
+        medicareId = medId;
+        parent = nullptr;
     }
-    TreeNode(TreeNode* parentPtr, const string& medId) 
-    { 
-        medicareId = medId; 
-        parent = parentPtr; 
+    TreeNode(TreeNode *parentPtr, const string &medId)
+    {
+        medicareId = medId;
+        parent = parentPtr;
     };
 };
 
 class ContactTrace
 {
 private:
-
 public:
     ContactTrace()
     {
-
     }
 
     ~ContactTrace()
     {
-
     }
 
     bool IsEmpty()
     {
-
     }
 
     int GetSize()
     {
-
     }
 
-    void AddPatient0(const string&)
+    void AddPatient0(const string &)
     {
-
     }
 
-    void AddContact(const string&, const string&)
+    void AddContact(const string &, const string &)
     {
-
     }
 
-    TreeNode* LookUpContact(const string&)
+    TreeNode *LookUpContact(const string &)
     {
-
     }
 
-    void DeleteContact(const string&)
+    void DeleteContact(const string &)
     {
-
     }
 
-    void DisplayContact(TreeNode*)
+    void DisplayContact(TreeNode *)
     {
-
     }
 
-    void DisplayContact(const string&)
+    void DisplayContact(const string &)
     {
-
     }
 
-    void TraceSource(const string&)
+    void TraceSource(const string &)
     {
-
     }
 
-    void PrintContactCases(const string&)
+    void PrintContactCases(const string &)
     {
-
     }
 
     void PrintContactTree()
     {
-
     }
 
     void PrintHierarchicalTree()
     {
-
     }
-
 };
-
 
 int main()
 {
