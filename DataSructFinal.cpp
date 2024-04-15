b683576a9a0ed963121b50f5dd0c476d2ba66798
MigoCP <135284017+MigoCP@users.noreply.github.com>
2024-04-15 12:38:24 -0400

    Creation of main file DataStructuresFinal.cpp

---
+++ DataSructFinal.cpp
@@ -0,0 +1,108 @@
+#include <iostream>
+
+using namespace std;
+
+struct TreeNode
+{
+private:
+    string medicareId;
+    int directContacts = 0;
+    int totalCases = 1;
+    TreeNode* parent;
+    //list<TreeNode> tree;
+
+public:
+    TreeNode(const string& medId) 
+    { 
+        medicareId = medId; 
+        parent = nullptr; 
+    }
+    TreeNode(TreeNode* parentPtr, const string& medId) 
+    { 
+        medicareId = medId; 
+        parent = parentPtr; 
+    };
+};
+
+class ContactTrace
+{
+private:
+
+public:
+    ContactTrace()
+    {
+
+    }
+
+    ~ContactTrace()
+    {
+
+    }
+
+    bool IsEmpty()
+    {
+
+    }
+
+    int GetSize()
+    {
+
+    }
+
+    void AddPatient0(const string&)
+    {
+
+    }
+
+    void AddContact(const string&, const string&)
+    {
+
+    }
+
+    TreeNode* LookUpContact(const string&)
+    {
+
+    }
+
+    void DeleteContact(const string&)
+    {
+
+    }
+
+    void DisplayContact(TreeNode*)
+    {
+
+    }
+
+    void DisplayContact(const string&)
+    {
+
+    }
+
+    void TraceSource(const string&)
+    {
+
+    }
+
+    void PrintContactCases(const string&)
+    {
+
+    }
+
+    void PrintContactTree()
+    {
+
+    }
+
+    void PrintHierarchicalTree()
+    {
+
+    }
+
+};
+
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
